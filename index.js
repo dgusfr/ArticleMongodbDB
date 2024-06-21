fff5660b261d4ba6c14243bb6701de26b35d906e
dgusfr <diegogustavo.fr@gmail.com>
2024-06-21 09:23:42 -0300

    final CRUD edit

--- index.js
+++ index.js
@@ -1,93 +1,40 @@
-// Importa o módulo mongoose, uma biblioteca ODM para MongoDB e Node.js
 const mongoose = require("mongoose");
+const Create = require("./Controllers/Create");
+const Read = require("./Controllers/Read");
+const Update = require("./Controllers/Update");
+const Delete = require("./Controllers/Delete");
 
-// Importa o esquema do modelo de artigo definido no arquivo Users.js
-const articleModel = require("./Models/Users");
-
-// Conecta ao MongoDB no localhost, utilizando o banco de dados myDatabase
-// As opções useNewUrlParser e useUnifiedTopology são usadas para evitar avisos de depreciação
 mongoose.connect("mongodb://localhost:27017/myDatabase", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Cria um modelo chamado "Article" baseado no esquema importado de articleModel
-const Article = mongoose.model("Article", articleModel);
+const create = new Create();
+const read = new Read();
+const update = new Update();
+const deleteOp = new Delete();
 
-// Cria uma nova instância do modelo Article com os dados especificados
-const review = new Article({
+const articleData = {
   title: "Book Review",
   author: "Gustavo Costa",
   body: "This is a review of a book.",
   resume: { content: "This is a review", author: "Gustavo Costa" },
-});
+};
 
-// Salva a nova instância (documento) no banco de dados MongoDB
-review
-  .save()
-  .then(() => {
-    console.log("Review saved");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Criar um artigo
+create.createArticle(articleData);
 
-// Encontra todos os documentos no banco de dados MongoDB de forma assíncrona
-async function findArticles() {
-  try {
-    const articles = await Article.find({});
-    console.log(articles);
-  } catch (err) {
-    console.error(err);
-  }
-}
+// Ler artigos
+read.findAllArticles();
+read.findArticleById("66756973b8b1fb0324746f01");
+read.findArticlesByAuthor("John Doe");
+read.findOneArticleByAuthor("John");
 
-//Busca elementos pelo id
-Article.find({ _id: "66756973b8b1fb0324746f01" })
-  .then((article) => {
-    console.log(article);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//Busca elementos por conteudo
-Article.find({ author: "John Doe" })
-  .then((article) => {
-    console.log(article);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//findOne busca apenas um elemento
-Article.findOne({ author: "John" })
-  .then((article) => {
-    console.log(article);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//Deletando um elemento
-Article.findByIdAndDelete("66756973b8b1fb0324746f01")
-  .then(() => {
-    console.log("Article deleted");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-//Atualizando um elemento
-Article.findByIdAndUpdate("66756973b8b1fb0324746f02", {
+// Atualizar um artigo
+update.updateArticleById("66756973b8b1fb0324746f02", {
   title: "Vue and React",
   author: "José",
-})
-  .then(() => {
-    console.log("Article updated");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+});
 
-findArticles();
+// Deletar um artigo
+deleteOp.deleteArticleById("66756973b8b1fb0324746f01");
